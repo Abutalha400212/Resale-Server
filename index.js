c8fefce7446f3a1ebafee8d2d9d480f1b39d691b
Abu Talha <abutalha141999@gmail.com>
2022-11-25 00:40:12 +0600

    jwt token generate and admin seller route create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -16,6 +17,69 @@ const client = new MongoClient(url, {
 async function runRellerDb() {
   try {
     console.log("db connected");
+    const categoriesItemCollection = client
+      .db("reseller-market")
+      .collection("categories-item");
+    const userCollection = client.db("reseller-market").collection("users");
+    app.get("/category", async (req, res) => {
+      const query = {};
+      const result = await categoriesItemCollection
+        .find(query)
+        .project({ brand: 1 })
+        .toArray();
+      res.send(result);
+    });
+    app.get("/categoriesItem", async (req, res) => {
+      const brand = req.query.brand;
+      const query = { brand: brand };
+      const result = await categoriesItemCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/categoriesItemDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await categoriesItemCollection.findOne({
+        _id: ObjectId(id),
+      });
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get('/users/admin/:email',async(req,res)=>{
+      const email = req.params.email
+      const query = {email:email}
+      const user = await userCollection.findOne(query)
+      res.send({isAdmin: user?.account === 'admin'})
+    })
+    app.get('/users/seller/:email',async(req,res)=>{
+      const email = req.params.email
+      const query = {email:email}
+      const user = await userCollection.findOne(query)
+      res.send({isSeller: user?.account === 'seller'})
+    })
+    app.get('/users/user',async(req,res)=>{
+      const account = req.query.account
+      const query = {account: account}
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      if (user) {
+        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {
+          expiresIn: "1d",
+        });
+        return res.send({
+          success: true,
+          accessToken: token,
+        });
+      }
+      res.send({status:'unAuthorized Access'})
+    });
   } finally {
   }
 }
