6e8de52e3678bc6a5fe664320ba4ba6fcb7338a2
Abu Talha <abutalha141999@gmail.com>
2022-11-26 09:16:17 +0600

    new collection created and integrated

--- index.js
+++ index.js
@@ -21,18 +21,39 @@ async function runRellerDb() {
       .db("reseller-market")
       .collection("categories-item");
     const userCollection = client.db("reseller-market").collection("users");
+    const bookingCollection = client.db("reseller-market").collection("booking")
+    app.post('/booking',async(req,res)=>{
+      const result = await bookingCollection.insertOne(req.body)
+      res.send(result)
+    })
     app.get("/category", async (req, res) => {
-      const query = {};
-      const result = await categoriesItemCollection
-        .find(query)
-        .project({ brand: 1 })
-        .toArray();
-      res.send(result);
+      const result = await categoriesItemCollection.find({}).toArray();
+      const newItem = [...new Set(result.map((item) => item.brand))];
+      res.send(newItem);
     });
     app.get("/categoriesItem", async (req, res) => {
       const brand = req.query.brand;
       const query = { brand: brand };
       const result = await categoriesItemCollection.find(query).toArray();
+
+      res.send(result);
+    });
+    app.get('/allItem',async(req,res)=>{
+      const result = await categoriesItemCollection.find({}).toArray()
+      res.send(result)
+    })
+    app.put("/users/seller/:email", async (req, res) => {
+      const { email } = req.params;
+      const status = req.body.status;
+      const updateStatus = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await userCollection.updateOne(
+        {email:email},
+        updateStatus
+      );
       res.send(result);
     });
     app.get("/categoriesItemDetails/:id", async (req, res) => {
@@ -42,41 +63,70 @@ async function runRellerDb() {
       });
       res.send(result);
     });
+    app.post("/addCategoryItem", async (req, res) => {
+      const filter = req.body;
+      const result = await categoriesItemCollection.insertOne(filter);
+      res.send(result);
+    });
+    app.get("/myProducts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await categoriesItemCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/myProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await categoriesItemCollection.deleteOne({
+        _id: ObjectId(id),
+      });
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await userCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.get('/users/admin/:email',async(req,res)=>{
-      const email = req.params.email
-      const query = {email:email}
-      const user = await userCollection.findOne(query)
-      res.send({isAdmin: user?.account === 'admin'})
-    })
-    app.get('/users/seller/:email',async(req,res)=>{
-      const email = req.params.email
-      const query = {email:email}
-      const user = await userCollection.findOne(query)
-      res.send({isSeller: user?.account === 'seller'})
-    })
-    app.get('/users/user',async(req,res)=>{
-      const account = req.query.account
-      const query = {account: account}
-      const result = await userCollection.find(query).toArray()
-      res.send(result)
-    })
-    app.get('/users/seller',async(req,res)=>{
-      const account = req.query.account
-      const query = {account: account}
-      const result = await userCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ isAdmin: user?.account === "admin" });
+    });
+    app.get("/users/verified/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ isVerified: user?.status === "Verified" });
+    });
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ isSeller: user?.account === "seller" });
+    });
+    app.get("/users/user", async (req, res) => {
+      const account = req.query.account;
+      const query = { account: account };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/users/seller", async (req, res) => {
+      const account = req.query.account;
+      const query = { account: account };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
       const filter = { email: email };
       const user = await userCollection.findOne(filter);
       if (user) {
-        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
           expiresIn: "1d",
         });
         return res.send({
@@ -84,7 +134,7 @@ async function runRellerDb() {
           accessToken: token,
         });
       }
-      res.send({status:'unAuthorized Access'})
+      res.send({ status: "unAuthorized Access" });
     });
   } finally {
   }
